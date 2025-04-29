5242961ded2bc0424b5b7dce900abb3122dd176e
Yuliia Kushnir <lavrinenkoyuli@gmail.com>
2025-04-29 11:56:54 +0300

    Added activities functionality

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NgZorroAntdModule } from '../NgZorroAntdModule';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { RouterLink, RouterOutlet } from '@angular/router';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    NgZorroAntdModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterLink,
+    RouterOutlet,
+    HttpClientModule
+  ], 
+  exports:[
+    CommonModule,
+    NgZorroAntdModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterLink,
+    RouterOutlet,
+    HttpClientModule
+  ]
+})
+export class SharedModule { }
